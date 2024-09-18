6aa316d8c140fd523bcb7fae83a4a7200b37e157
Rafael Pinto <archlinuxrp@gmail.com>
2024-09-18 17:41:46 +0100

    Revert "testing support #1"
    This reverts commit 560fd5679b88c7bb41710020c0031065aec43930.

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node', // or 'jsdom' if you're testing in a browser-like environment
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest', // Transform TypeScript files
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
